2be56666d1e56fd42376b719ee86b43d40a0032d
ddandroid <programmer22@dynamicdreamz.com>
2016-08-12 10:19:00 +0530

    new variable added by chitrang

--- app/src/main/java/com/demo/dynamictablayout/MainActivity.java
+++ app/src/main/java/com/demo/dynamictablayout/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int programmer22;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
